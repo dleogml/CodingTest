3698aa2c12a5bed9a5c1bec8ccb72bec06457a60
dleogml <dleogmlrnt@naver.com>
2021-01-13 22:16:57 +0900

    2021.01.13 파일명 정렬, N진수 게임

--- programmers/level2/FileNameSort.java
+++ programmers/level2/FileNameSort.java
@@ -1,5 +1,8 @@
 package programmers.level2;
 
+import java.util.Arrays;
+import java.util.Comparator;
+
 // 파일명 정렬
 /*
 파일명은 크게 HEAD, NUMBER, TAIL의 세 부분으로 구성된다.
@@ -19,5 +22,55 @@ package programmers.level2;
  */
 // 위 기준에 따라 정렬된 배열을 출력
 public class FileNameSort {
+    class Solution {
+        public String[] solution(String[] files) {
+            Arrays.sort(files, new Comparator<String>() {
+                @Override
+                public int compare(String s1, String s2) {
+                    String[] file1 = div(s1);
+                    String[] file2 = div(s2);
 
+                    int headValue = file1[0].compareTo(file2[0]);
+
+                    if(headValue == 0) {
+                        int num1 = Integer.parseInt(file1[1]);
+                        int num2 = Integer.parseInt(file2[1]);
+
+                        return num1 - num2;
+                    } else {
+                        return headValue;
+                    }
+                }
+
+                private String[] div(String str) {
+                    String head = "";
+                    String number = "";
+                    String tail = "";
+
+                    int idx = 0;
+                    for( ; idx < str.length() ; ++idx) {
+                        char ch = str.charAt(idx);
+                        if(ch >= '0' && ch <= '9') break;
+                        head += ch;
+                    }
+
+                    for( ; idx < str.length() ; ++idx) {
+                        char ch = str.charAt(idx);
+                        if(!(ch >= '0' && ch <= '9')) break;
+                        number += ch;
+                    }
+
+                    for( ; idx < str.length() ; ++idx) {
+                        char ch = str.charAt(idx);
+                        tail += ch;
+                    }
+
+                    String[] file = {head.toLowerCase(), number, tail};
+
+                    return file;
+                }
+            });
+            return files;
+        }
+    }
 }
