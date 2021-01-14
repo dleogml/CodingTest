60306fe54a2b0e1bc63a148339b8098d578cd16e
dleogml <dleogmlrnt@naver.com>
2021-01-14 22:26:26 +0900

    2021.01.14 N진수 게임

--- programmers/level2/BaseNGame.java
+++ programmers/level2/BaseNGame.java
@@ -9,5 +9,50 @@ package programmers.level2;
 // 단, 10~15는 각각 대문자 A~F로 출력한다.
 // 미리 구할 숫자의 갯수는 앞에서 부터
 public class BaseNGame {
+    class Solution {
+        public String solution(int n, int t, int m, int p) {
+            StringBuilder answer = new StringBuilder("");
+            // 0부터 시작하기 때문에 0을 미리 넣어놓는다.
+            StringBuilder str = new StringBuilder("0");
 
+            // 16진수일 경우에 바꿔줄 알파벳들
+            String[] alpaForMoreThan10 = {"A", "B", "C", "D", "E", "F"};
+
+            int num = 1;
+
+            // 미리 구할 숫자의 갯수 * 인원의 길이까지 답이 나올만큼 반복한다.
+            while (str.length() - 1 < t * m) {
+
+                StringBuilder sub = new StringBuilder("");
+                // 몫, 1부터 시작
+                int quot = num;
+                // 나머지
+                int remain;
+
+                while (quot > 0) {
+                    remain = quot % n;
+                    quot = quot / n;
+
+                    // 알파벳으로 바꿔야 하는 경우
+                    if (remain >= 10 && remain <= 15) {
+                        sub.append(alpaForMoreThan10[remain-10]);
+                    } else {
+                        sub.append(remain);
+                    }
+                }
+                str.append(sub.reverse());
+                num++;
+            }
+
+            int tubePlace = p;
+
+            for (int i = 0; i < t; i++) {
+                answer.append(str.charAt(tubePlace - 1));
+                // 튜브 차례
+                tubePlace += m;
+            }
+
+            return answer.toString();
+        }
+    }
 }
